9c54c3216edad330991d9606790db1f94f807255
BigBear-01 <evanfernando785@gmail.com>
2026-05-13 08:23:36 +0700

    Perubahan soal satu

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -1,18 +1,19 @@
-let name = ``;
-let role = ``;
-
+let name = `Deco`;
+let role = `Marksman`;
 
 if (name === ``) {
     console.log(`You must had a name!`);
 }   else if (role === ``) {
-    console.log(`You must pick a role!`);
+    console.log(`${name}, Pick Your Role First!`);
 }   else if (role === `Knight`) {
-    console.log(`Halo Knight ${name} , kamu dapat menyerang!`);
-}   else if (role === `Healer`) {
-    console.log(`Halo Healer ${name} , Bantu temanku!`);
+    console.log(`Hello Knight ${name}, Help us with your power!`);
 }   else if (role === `Wizard`) {
-    console.log(`Halo Wizard ${name} , Ciptakan keajaiban!`);
-}   
+    console.log(`Hello wizard ${name}, Help us with your magic!`);
+}   else if (role === `Healer`) {
+    console.log(`Hello angel ${name}, Help us to heal our wounded troops`);
+}   else if (role === `Marksman`) {
+    console.log(`Hello Marksman ${name}, Help us to attack the enemy from afar!`);
+}
 else {
-    console.log(`Kamu belum milih role ya, yaudah kamu jadi bot aja deh!`);
+    console.log(`Seems like you havent pick any role, its ok you can be a bot then`);
 }
